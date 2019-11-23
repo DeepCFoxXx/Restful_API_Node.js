f2f2191b95685bbb59b15c67d232339eb00cae46
DeepCFoxXx <learncode23@gmail.com>
2019-11-23 15:59:23 +0000

    refactoring products.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,6 +7,8 @@ router.get('/', (req, res, next) => {
   });
 });
 
+//-------------------------------------------------------------------------------------------
+
 router.post('/', (req, res, next) => {
   const order = {
     productId: req.body.productId,
@@ -18,12 +20,16 @@ router.post('/', (req, res, next) => {
   });
 });
 
+//-------------------------------------------------------------------------------------------
+
 router.get('/:orderId', (req, res, next) => {
   res.status(200).json({
     message: 'Order details',
     orderId: req.params.orderId
   });
 });
+
+//-------------------------------------------------------------------------------------------
 
 router.delete('/:orderId', (req, res, next) => {
   res.status(200).json({
