051a40952245e19c56a74ab07979cd580170530a
DeepCFoxXx <learncode23@gmail.com>
2019-11-30 18:20:07 +0000

    started on get + post for orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,29 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Order = require('../models/order');
+const Order = require("../models/order");
+const Product = require("../models/product");
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Orders were fetched'
-  });
+router.get("/", (req, res, next) => {
+  Order.find()
+    .select("product quantity _id")
+    .exec()
+    .then(doc => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            product: doc.quantity,
+            quantity: doc.quantitiy,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/orders/" + doc._id
+            }
+          };
+        })
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        erroe: err
+      });
+    });
 });
 
 //-------------------------------------------------------------------------------------------
 
-router.post('/', (req, res, next) => {
-  const order = new Order({
-    _id: mongoose.Types.ObjectId(),
-    quantity: req.body.quantity,
-    product: req.body.productId 
-  });
-  order
-    .save()
-    .exec()
+router.post("/", (req, res, next) => {
+  Product.findById(req.body.productId)
+    .then(product => {
+      const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+      });
+      return order.save();
+    })
     .then(result => {
       console.log(result);
-      res.status(201).json(result);
+      res.status(201).json({
+        message: "Orders Stored",
+        createdOrder: {
+          _id: result._id,
+          product: result.product,
+          quantity: result.quantity
+        },
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/orders/" + result._id
+        }
+      });
     })
     .catch(err => {
       console.log(err);
@@ -35,20 +69,20 @@ router.post('/', (req, res, next) => {
 
 //-------------------------------------------------------------------------------------------
 
-router.get('/:orderId', (req, res, next) => {
+router.get("/:orderId", (req, res, next) => {
   res.status(200).json({
-    message: 'Order details',
+    message: "Order details",
     orderId: req.params.orderId
   });
 });
 
 //-------------------------------------------------------------------------------------------
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete("/:orderId", (req, res, next) => {
   res.status(200).json({
-    message: 'Order was deleted',
+    message: "Order was deleted",
     orderId: req.params.orderId
   });
 });
 
-module .exports = router;
+module.exports = router;
